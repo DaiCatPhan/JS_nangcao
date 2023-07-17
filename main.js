76071b3ac39eeb823f6c2105c1ad6dba53f20363
Dai Cat <phandaicat12032002@gmail.com>
2023-07-17 13:44:54 +0700

    8. Khái niệm Closure? _ Ứng dụng logger()

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
-function makeCounter(){
-    let count = 0;
+// Ứng dụng closure logger()
 
-    function increase(){
-        return ++ count;
+function createLogger(namespace){
+
+    function logger(message){
+        console.log(`[${namespace}] ${message}`);
     }
-
-    return increase();
-
+    return logger
 }
 
-const increase1 = makeCounter();
-console.log( increase1);
+// ============== App ===============
+
+// Ex Register.js
+
+const infoLogger = createLogger('Info')
+
+infoLogger('Bat dau gui mail')
+infoLogger('Gui mail loi , gui lai !!!')
+infoLogger('Gui mail thanh cong')
+
+// ForgotPassword.js
+
+const errorLogger = createLogger('Error');
+
+errorLogger('Email khong ton tai trong DB')
+errorLogger('Gui mai thanh cong')
