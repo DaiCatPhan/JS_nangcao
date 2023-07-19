4ba3eb76ee9d00ef82a304518c0c3f9f0b288925
Dai Cat <phandaicat12032002@gmail.com>
2023-07-19 10:39:21 +0700

    21. Fn.call() method_Kỉ thuật mượn hàm

--- main.js
+++ main.js
@@ -1,69 +1,21 @@
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
+const teacher = {
+    firstName: 'Minh ',
+    lastName: 'Thu',
+}
 
-
-const app = (() => {
-    const cars = ['BMW' , 'Mes' , "Kia"];
-
-    const root = $('#root')
-    const input = $('#input')
-    const submit = $('#submit')
-    return {
-        add(car){
-            cars.push(car)
-        },
-
-        delete(index){
-            cars.splice(index,1)
-        },
-
-        render(){
-            const html = cars.map((car , index) => {
-                return `
-                    <li>
-                        ${car}
-                        <span data-index="${index}" class="delete">&times</span>
-                    </li>
-                `
-            }).join('')
-
-            root.innerHTML = html;
-        },
-
-        handleDelete(e){
-            const deleteBtn = e.target.closest('.delete');
-            
-            if(deleteBtn){
-                const index = e.target.dataset.index;
-                this.delete(index)
-                this.render()
-            }
-        },
-
-        init(){
-            // Handle DOM events
-            submit.onclick = () => {
-                const car = input.value;
-                this.add(car);
-                this.render();
-
-                input.value = '';
-                input.focus();
-            }
-
-            root.onclick = this.handleDelete.bind(this)
-
-
-            // arrow function k có context nên this là app
-            // nếu k phải arrow function thì this là submit
-
-
-            this.render()
-        }
+const me = {
+    firstName: 'Son',
+    lastName: 'Dang',
+    showFullName(){
+        console.log(`${this.firstName} ${this.lastName}`);
     }
-})()
+}
 
-console.log(app);
-app.init()
+me.showFullName.call(teacher)
+
+// Kỉ thuật mượn hàm showFullName cho teacher 
 
 
+
+// bind chỉ bind this rồi trả ra cái hàm mới, this mới
+// call là by this rồi gọi luôn cái hàm đó
